51dff2d275c27dcd0e7f63fc777a9645e41e65d3
iv4n-t3a <iv4n.t3a@yandex.ru>
2025-08-29 01:11:31 +0300

    Add shift and caps lock support to keyboard driver

--- kernel/drivers/ps2_keyboard.c
+++ kernel/drivers/ps2_keyboard.c
@@ -1,21 +1,61 @@
 #include "kernel/drivers/ps2_keyboard.h"
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #include "kernel/drivers/keyboard.h"
 #include "kernel/libs/stdio.h"
 #include "kernel/shell/shell.h"
 
 static const char scan_code_table[] = "??1234567890-=\b"
                                       "?qwertyuiop[]"
-                                      "\n?asdfgh"
-                                      "jkl;\'`?\\zxcv"
-                                      "bnm,./??? ";
+                                      "\n?asdfghjkl;\'`"
+                                      "?\\zxcvbnm,./??? ";
+
+static const char scan_code_table_shift[] = "??!@#$%^&*()_+\b"
+                                            "?QWERTYUIOP{}"
+                                            "\n?ASDFGHJKL:\"~"
+                                            "?|ZXCVBNM<>???? ";
+
+typedef enum {
+  KEY_LEFT_SHIFT_PRESSED = 0x2A,
+  KEY_RIGHT_SHIFT_PRESSED = 0x36,
+  KEY_LEFT_SHIFT_RELEASED = 0xAA,
+  KEY_RIGHT_SHIFT_RELEASED = 0xB6,
+  KEY_CAPS_LOCK_PRESSED = 0x3A,
+  KEY_CAPS_LOCK_RELEASED = 0xBA,
+} special_key_t;
+
+typedef struct {
+  bool shift;
+  bool caps_lock;
+} keyboard_state_t;
+
+keyboard_state_t state;
 
 void ps2_command_hook(uint8_t scan_code) {
-  if (scan_code > sizeof(scan_code_table)) {
-    return;
+  switch (scan_code) {
+  case KEY_CAPS_LOCK_PRESSED:
+    state.caps_lock = !state.caps_lock;
+    break;
+  case KEY_LEFT_SHIFT_PRESSED:
+  case KEY_RIGHT_SHIFT_PRESSED:
+    state.shift = true;
+    break;
+  case KEY_LEFT_SHIFT_RELEASED:
+  case KEY_RIGHT_SHIFT_RELEASED:
+    state.shift = false;
+    break;
+  default:
+    if (scan_code > sizeof(scan_code_table)) {
+      return;
+    }
+
+    char c = state.shift ^ state.caps_lock ? scan_code_table_shift[scan_code]
+                                           : scan_code_table[scan_code];
+    keycode_t keycode = {.pressed_char = c};
+    // TODO: multiple tty, device files and propper drivers organization
+    kernel_shell_process_char(keycode);
+    break;
   }
-  char c = scan_code_table[scan_code];
-  keycode_t keycode = {.pressed_char = c};
-  // TODO: multiple tty, device files and propper drivers organization
-  kernel_shell_process_char(keycode);
 }
